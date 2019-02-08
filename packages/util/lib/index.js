24fca208f3471e0cd223aedc13240b4b13f34ea6
Josh Robinson <hexorx@gmail.com>
2019-02-08 11:25:53 -0700

    Added index.js to util pkg

---
+++ packages/util/lib/index.js
@@ -0,0 +1,9 @@
+const Util = {
+  r: require('ramda'),
+  bn: require('bn.js'),
+  buf: require('./buf'),
+  Writer: require('./writer'),
+  Reader: require('./reader')
+};
+
+module.exports = Util;
---
+++ packages/util/test/index.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Util = require('../lib');
+
+describe('Reader', () => {
+  it('should include all utils', () => {
+    expect(Util).to.have.all.keys('r', 'bn', 'buf', 'Writer', 'Reader');
+  });
+});
