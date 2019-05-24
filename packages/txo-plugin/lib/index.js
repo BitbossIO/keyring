0fbf2c4c5f8849f181d60a769735778c56100ca0
Josh Robinson <hexorx@gmail.com>
2019-05-24 14:03:18 -0600

    chore: change txo-plugin name and version to come from package

--- packages/txo-plugin/lib/index.js
+++ packages/txo-plugin/lib/index.js
@@ -1,11 +1,13 @@
 const Plugin = require('@keyring/plugin');
 const TransactionFacet = require('./transaction');
 
+const Package = require('../package.json');
+
 class TXOPlugin extends Plugin.Provider {
   static get id() { return 'txo'; }
   static get name() { return 'TXO Plugin'; }
-  static get description() { return 'none'; }
-  static get version() { return 'none'; }
+  static get description() { return Package.description; }
+  static get version() { return Package.version; }
 
   static get facets() {
     return { transaction: TransactionFacet };
